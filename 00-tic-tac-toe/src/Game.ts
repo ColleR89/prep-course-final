c2d67c4f00c2e2bb94ef2fc5bd135da233eddddd
Andrejs Lavrinovics <andrew.lavrinovich@inbox.lv>
2023-04-09 18:00:08 +0000

    tictactoe - done

--- 00-tic-tac-toe/src/Game.ts
+++ 00-tic-tac-toe/src/Game.ts
@@ -1,27 +1,63 @@
 export type XO = "X" | "O" | "-";
 
 export class Game {
+  whosTurn: XO = "X";
+  gameCells: XO[] = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
+
   getCells(): XO[] {
-    return ["X", "-", "-", "-", "-", "-", "-", "-", "-"];
+    return this.gameCells;
   }
 
   getTurn(): XO {
-    return "X";
+    return this.whosTurn;
   }
 
   getWinner(): XO {
+    let c: number = 1;
+    let t: XO;
+    let b: XO[] = this.gameCells;
+    while (c <= 2) {
+      if (c == 1) t = "O";
+      else t = "X";
+      if (
+        // horizontal
+        (b[0] == t && b[1] == t && b[2] == t) ||
+        (b[3] == t && b[4] == t && b[5] == t) ||
+        (b[6] == t && b[7] == t && b[8] == t) ||
+        // vertical
+        (b[0] == t && b[3] == t && b[6] == t) ||
+        (b[1] == t && b[4] == t && b[7] == t) ||
+        (b[2] == t && b[5] == t && b[8] == t) ||
+        // diagonal
+        (b[0] == t && b[4] == t && b[8] == t) ||
+        (b[2] == t && b[4] == t && b[6] == t)
+      ) {
+        return t;
+      }
+      c++;
+    }
     return "-";
   }
 
   isTie(): boolean {
-    return false;
+    if (this.gameCells.find((elem) => elem === "-")) {
+      return false;
+    } else {
+      return true;
+    }
   }
 
   onClick(i: number): void {
-    console.log(`cell ${i} clicked`);
+    if (this.gameCells[i] === "-") {
+      this.gameCells[i] = this.whosTurn;
+      this.whosTurn === "X" ? (this.whosTurn = "O") : (this.whosTurn = "X");
+      console.log(`cell ${i} clicked`);
+    }
   }
 
   restart(): void {
+    this.gameCells = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
+    this.whosTurn = "X";
     console.log("restart called");
   }
 }
