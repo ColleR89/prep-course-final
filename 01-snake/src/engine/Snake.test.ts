dd66a09328acb411cb86e1c0f966ca05a80256fc
Andrejs Lavrinovics <andrew.lavrinovich@inbox.lv>
2023-04-13 18:04:27 +0000

    uzsāku čūsku

--- 01-snake/src/engine/Snake.test.ts
+++ 01-snake/src/engine/Snake.test.ts
@@ -8,4 +8,24 @@ describe("Snake", () => {
     expect(snake.getHead()).toEqual(new Cell(2, 0));
     expect(snake.getTail()).toEqual([new Cell(0, 0), new Cell(1, 0)]);
   });
+
+  it("should be able to move RIGHT", () => {
+    const snake = new Snake();
+
+    snake.move();
+
+    expect(snake.getHead()).toEqual(new Cell(3, 0));
+    expect(snake.getTail()).toEqual([new Cell(1, 0), new Cell(2, 0)]);
+  });
+
+  it("should be able to move DOWN", () => {
+    const snake = new Snake();
+
+    snake.setDirection("Down");
+    expect(snake.getDirection()).toEqual("Down");
+    snake.move();
+
+    expect(snake.getHead()).toEqual(new Cell(2, 1));
+    expect(snake.getTail()).toEqual([new Cell(1, 0), new Cell(2, 0)]);
+  });
 });
--- 01-snake/src/engine/Snake.ts
+++ 01-snake/src/engine/Snake.ts
@@ -2,22 +2,37 @@ import { Cell } from "./Cell";
 import { Direction } from "./Direction";
 
 export class Snake {
-  setDirection(direction: Direction) {}
+  head: Cell = new Cell(2, 0);
+  tail: Cell[] = [new Cell(0, 0), new Cell(1, 0)];
+  direction: Direction = "Right";
 
-  move() {}
+  setDirection(newDirection: Direction) {
+    this.direction = newDirection;
+  }
+
+  move() {
+    const oldHead = this.getHead();
+    if (this.getDirection() === "Right") {
+      this.head = new Cell(oldHead.x + 1, oldHead.y);
+    } else if (this.getDirection() === "Down") {
+      this.head = new Cell(oldHead.x, oldHead.y + 1);
+    }
+    this.tail.shift();
+    this.tail.push(new Cell(oldHead.x, oldHead.y));
+  }
 
   grow() {}
 
   getHead(): Cell {
-    return new Cell(4, 0);
+    return this.head;
   }
 
   getDirection(): Direction {
-    return "Right";
+    return this.direction;
   }
 
   getTail(): Cell[] {
-    return [new Cell(0, 0), new Cell(2, 0)];
+    return this.tail;
   }
 
   isTakenBySnake(cell: Cell): boolean {
